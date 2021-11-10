47c26b4b0dfdaf452ede1bf51f0a1379ffd0c55a
shraddha-23-deshpande <deshpandeshraddha2311@gmail.com>
2021-11-10 12:46:08 +0530

    completed

--- out/app.js
+++ out/app.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-//const router = express.Router()
 var app = (0, express_1.default)();
 app.use(express_1.default.json());
 var routes = require('./router');
